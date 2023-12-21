28c4ff597a82cfb304823f13c0dea718ab4e0b79
James Buchanan <james42buchanan@gmail.com>
2023-12-21 15:59:16 +0000

    Added several of the completed exercises.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a follow-along of the exercises from the Free Code Camp and Treehouse algorithms and datastructures lesson in Python.
---
+++ arrays.py
@@ -0,0 +1,17 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list:
+    print(True)
+
+for number in new_list:
+    if number == 1:
+        print(True)
+    break
+
+numbers = []
+numbers.append(2)
+numbers.append(200)
+numbers 
+
+print(len(numbers))
